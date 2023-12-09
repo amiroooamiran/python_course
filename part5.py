6fabc4155a8a3e4c888b205dc6f3c31756c4f16a
amirhossein amiran <93470832+amiroooamiran@users.noreply.github.com>
2023-12-09 08:22:03 +0330

    if and else in python

---
+++ part5.py
@@ -0,0 +1,9 @@
+fnum = input("what is first number ")
+snum = input("what is second number ")
+
+if fnum > snum:
+    print(f' first number is: {fnum} larger!')
+elif snum > fnum:
+    print(f' second number is: {snum} larger!')
+else:
+    print("=============")
