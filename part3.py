35971eefca8251f40712dcad80166717ae38fba3
amirhossein amiran <93470832+amiroooamiran@users.noreply.github.com>
2023-12-09 08:18:20 +0330

    variabels in python

---
+++ part3.py
@@ -0,0 +1,7 @@
+# geting basic information from user
+fname = input("what is your name? ")
+lname = input("what is your last name? ")
+
+print(f"your name is {fname} {lname}") #FST
+
+# fname lname pname bcity
