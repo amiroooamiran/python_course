60e93f0d951f296d40c87eac7f9080dd71b5c199
AmirhosseinAmiran <amiran.amirhossein@gmail.com>
2024-03-21 18:14:36 +0330

    create encrypted messages

--- part8.py
+++ part8.py
@@ -1,5 +1,27 @@
-import hashlib
+from cryptography.fernet import Fernet
 
-string_to_hash = "Hello, amirooo"
-sha256 = hashlib.sha256(string_to_hash.encode()).hexdigest()
-print(sha256)
+def generate_key():
+    return Fernet.generate_key()
+
+def encrypt_message(message, key):
+    cipher_suite = Fernet(key)
+    encrypted_message = cipher_suite.encrypt(message.encode())
+    return encrypted_message
+
+def decrypt_message(enctypted_message, key):
+    cipher_suite = Fernet(key)
+    decrypted_message = cipher_suite.decrypt(enctypted_message).decode()
+    return decrypted_message
+
+if __name__ == "__main__":
+    key = generate_key()
+    print("Generate Key: ", key)
+
+    message = "Hello, amirooo"
+
+    encrypted_message = encrypt_message(message, key)
+    print("Encrypted Message: ", encrypted_message)
+
+    decrypted_message = decrypt_message(encrypted_message, key)
+    print("Encrypt Message: ", decrypted_message)
+
